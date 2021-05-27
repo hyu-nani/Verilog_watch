825de6fe3d252015178426ea88b786b563a67fba
hyu nani <60097661+hyu-nani@users.noreply.github.com>
2021-05-28 00:46:55 +0900

    BCD 코드 약간 수정

--- BCD.v
+++ BCD.v
@@ -18,18 +18,18 @@ module	BCD(
 		
 		for (i=7;i>=0;i=i-1) begin
 			if(hundreds	>= 5)
-				hundreds = hundreds + 3;
+				hundreds <= hundreds + 4'b0011;
 			if(tens >= 5)
-				tens = tens + 3;
+				tens 		<= tens + 4'b0011;
 			if(ones >= 5)
-				ones = ones + 3;
+				ones 		<= ones + 4'b0011;
 				
-			hundreds = hundreds << 1;
-			hundreds[0] = tens[3];
-			tens	=	tens	<< 1;
-			tens[0] = ones[3];
-			ones = ones << 1;
-			ones[0] = bin[i];
+			hundreds 	<= hundreds << 1;
+			hundreds[0] <= tens[3];
+			tens			<=	tens	<< 1;
+			tens[0] 		<= ones[3];
+			ones 			<= ones << 1;
+			ones[0] 		<= bin[i];
 		end
 	end
 	
