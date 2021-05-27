b07ddebbf2af4daa1261e438115058f6dc781659
hyu nani <60097661+hyu-nani@users.noreply.github.com>
2021-05-28 01:00:22 +0900

    재업데이트2

--- clock.v
+++ clock.v
@@ -11,14 +11,14 @@ module clock(
 	input 	clk, rst;
 	
 	output	[11:0]year;
-	output	[3:0]	month;
-	output	[4:0]	day;
-	output 	[5:0] second,minute,hour;
+	output	[7:0]	month;
+	output	[7:0]	day;
+	output 	[7:0] second,minute,hour;
 	
-	reg	[11:0]year;
-	reg	[3:0]	month;
-	reg	[4:0]	day;
-	reg 	[5:0] second,minute,hour;		
+	reg		[11:0]year;
+	reg		[7:0]	month;
+	reg		[7:0]	day;
+	reg 		[7:0] second,minute,hour;		
 	
 	wire			clk1;
 	
@@ -30,36 +30,33 @@ module clock(
 	always @(posedge clk1 or negedge rst) begin
 		if(!rst)	begin
 			year		<=	12'd2021;
-			month		<=	4'd1;
-			day		<=	5'd1;
-			second	<= 6'd0;
-			minute	<=	6'd0;
-			hour		<=	6'd0;
+			month		<=	8'd1;
+			day		<=	8'd1;
+			second	<= 8'd0;
+			minute	<=	8'd0;
+			hour		<=	8'd0;
 		end
 		else begin
-			if(day	==	5'd30) begin
-				month	<=	month	+ 1'b1;
-				day	<=	5'd1;
-			end
-			if(month	==	4'd12) begin
-				year	<=	year + 1'b1;
-				month	<=	4'd1;
-				day	<=	5'd1;
-			end
-			if(second == 6'd59) begin 
-				second 	<= 6'd0;
+			if(second == 8'd59) begin 
+				second 	<= 8'd0;
 				minute	<=	minute + 1'b1;
 			end
-			if(minute == 6'd59) begin 
-				minute	<= 6'd0;
+			if(minute == 8'd59) begin 
+				minute	<= 8'd0;
 				hour		<=	hour + 1'b1;
 			end
-			if(hour	==	6'd23) begin
-				second	<= 6'd0;
-				minute	<=	6'd0;
-				hour		<=	6'd0;
+			if(hour	==	8'd23) begin
+				hour		<=	8'd0;
 				day		<=	day + 1'b1;
 			end 
+			if(day	==	8'd30) begin
+				day	<=	8'd1;
+				month	<=	month	+ 1'b1;
+			end
+			if(month	==	8'd12) begin
+				month	<=	8'd1;
+				year	<=	year + 1'b1;
+			end
 			second	<= second + 1'b1;
 		end
 			
